d68ef8d6ba028cfcc356fcd288b25284f5ced8cc
源星梦影 <qiyuanyxy@163.com>
2020-04-10 15:05:08 +0800

    项目初步搭建完成

---
+++ gmall-admin/src/main/java/com/atguigu/GmallAdminMainApplication.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2016-2019 谷粒开源 All rights reserved.
+ *
+ * <p>https://www.guli.cloud
+ *
+ * <p>版权所有，侵权必究！
+ */
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminMainApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GmallAdminMainApplication.class, args);
+  }
+}
